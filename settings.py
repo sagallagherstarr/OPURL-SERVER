52f3334f171f564890e3e7de5076ea252d464751
Scott Gallagher-Starr <scott.gallagherstarr@gmail.com>
2012-08-04 17:15:36 -0700

    Adding in toplevel files from existing project

--- settings.py
+++ settings.py
@@ -3,16 +3,18 @@
 """
 A sample of kay settings.
 
-:Copyright: (c) 2009 Accense Technology, Inc. 
+:Copyright: (c) 2009 Accense Technology, Inc.
                      Takashi Matsuo <tmatsuo@candit.jp>,
                      All rights reserved.
 :license: BSD, see LICENSE for more details.
 """
 
-DEFAULT_TIMEZONE = 'Asia/Tokyo'
+import os, os.path
+
+DEFAULT_TIMEZONE = 'Americas/Los_Angeles'
 DEBUG = True
 PROFILE = False
-SECRET_KEY = 'ReplaceItWithSecretString'
+SECRET_KEY = 'Sku^^pe7Y%%ar(secretts&trngi'
 SESSION_PREFIX = 'gaesess:'
 COOKIE_AGE = 1209600 # 2 weeks
 COOKIE_NAME = 'KAY_SESSION'
@@ -21,12 +23,22 @@ ADMINS = (
 )
 
 TEMPLATE_DIRS = (
+  'templates',
+##  'media/html',
+##  'media/templates',
+##  'client/templates',
 )
 
 USE_I18N = False
 DEFAULT_LANG = 'en'
 
+SESSION_STORE = 'kay.sessions.sessionstore.GAESessionStore'
+AUTH_USER_MODEL = 'kay.auth.models.GoogleUser'
+
 INSTALLED_APPS = (
+  'kay.sessions',
+  'kay.auth',
+  'client',
 )
 
 APP_MOUNT_POINTS = {
@@ -37,4 +49,9 @@ CONTEXT_PROCESSORS = (
   'kay.context_processors.request',
   'kay.context_processors.url_functions',
   'kay.context_processors.media_url',
+)
+
+MIDDLEWARE_CLASSES = (
+  'kay.sessions.middleware.SessionMiddleware',
+  'kay.auth.middleware.AuthenticationMiddleware',
 )
